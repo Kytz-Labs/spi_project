6d975b625346b35fed2ceeffc457158462ef6b14
blackwood-star <woodblack686@gmail.com>
2024-10-27 22:37:49 +0530

    navigation menu completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: cloudflare(), // Cloudflare Workers adapter
-  });
+  output: "server",
+
+  // Cloudflare Workers adapter
+  adapter: cloudflare(),
+
+  integrations: [tailwind()]
+});
