3eb90f80733b6346fac48118ca4ca38170333bfa
Lourdu-001 <lourdushelton@gmail.com>
2024-11-28 13:27:46 +0530

    brochures added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
   integrations: [tailwind(), react()],
 });
